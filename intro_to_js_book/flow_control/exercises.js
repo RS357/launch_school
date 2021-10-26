ed2635484c8438742281a76ddc601966aa180a76
Riz Syed <rizwansyed876@gmail.com>
2021-10-26 09:22:49 +0100

    new files added

--- intro_to_js_book/flow_control/exercises.js
+++ intro_to_js_book/flow_control/exercises.js
@@ -2,15 +2,15 @@
 // false || (true && false); -> false
 // true || (1 + 2);          -> true
 // (1 + 2) || true;          -> 3
-// true && (1 + 2);          -> 3  
-// false && (1 + 2);         -> false 
+// true && (1 + 2);          -> 3
+// false && (1 + 2);         -> false
 // (1 + 2) && true;          -> true 
 // (32 * 4) >= 129;          -> false 
 // false !== !true;          -> false 
 // true === 4;               -> false 
 // false === (847 === '847');-> true 
 // false === (847 == '847'); -> false 
-// (!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false; -> true
+// (false || (false) || (true) || false; -> true
 
 // 2) 3)
 // function evenOrOdd(number) {
@@ -28,10 +28,10 @@
 
 // 5)
 // if (foo()) {
-//   return "bar";
-// }
+//    return "bar";
+// } 
 // else {
-//   return quz();
+//   return qux();
 // }
 
 // 6) "Not Empty"
--- intro_to_js_book/loops_and_iterating/exercises.js
+++ intro_to_js_book/loops_and_iterating/exercises.js
@@ -15,4 +15,40 @@
 // console.log(factorial(7));     // => 5040
 // console.log(factorial(8));     // => 40320
 
-//3) 
+// 3) The while condition will always return true when the 1 is assigned to counter. 
+//   Also the test on line 7 never passes i.e. counter is never greater than 2.
+
+// 4) It produces no error and sends 5 to the console. 
+
+// 5) 
+
+// function randomNumberBetween(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// let tries = 0;
+// let result;
+
+// do {
+//   result = randomNumberBetween(1, 6);
+//   tries += 1;
+// } while (result <= 2);
+
+// console.log('It took ' + String(tries) + ' tries to get a number greater than 2');
+
+// 6) 
+
+let factorial = (num) => {
+  if (num === 1) return num;
+
+  return num * factorial(num - 1)
+}
+
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
