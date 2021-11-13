b2a01c5104c67e13160bbe52ae18d2ea4420c927
Riz Syed <rizwansyed876@gmail.com>
2021-11-13 20:32:59 +0000

    file added

--- exercises/js101_small_problems/easy1.js
+++ exercises/js101_small_problems/easy1.js
@@ -1,5 +1,70 @@
+let readlineSync = require("readline-sync");
 // Isn't it odd?
 // let isOdd = num => (Math.abs(num) % 2) !== 0
 
 // Odd Numbers
-for (let i = 1; i <= 99; i = i + 2) console.log(`${i}`)
+// for (let i = 1; i <= 99; i = i + 2) console.log(`${i}`)
+
+// How big is the room?
+// const SQ_METERS_TO_SQ_FEET = 10.7639
+// console.log("Would you like to input your measurements in feet or meters?")
+// let units = readlineSync.prompt()
+
+// console.log(`Please enter the length of the room in ${units}:`);
+// let length = readlineSync.prompt()
+
+// console.log(`Please enter the width of the room in ${units}:`);
+// let width = readlineSync.prompt()
+
+// length = Number(length);
+// width = Number(width); 
+
+// if (units === "feet") {
+//   let squareFeet = length * width; 
+//   let squareMeters = squareFeet / SQ_METERS_TO_SQ_FEET
+//   squareFeet = squareFeet.toFixed(2);
+//   squareMeters = squareMeters.toFixed(2);
+//   console.log(`The area of the room is ${squareFeet} square feet (${squareMeters} square meters).`)
+
+// } else if (units === "meters") {
+//   let squareMeters = length * width; 
+//   squareFeet = squareMeters * SQ_METERS_TO_SQ_FEET ;
+//   squareMeters = squareMeters.toFixed(2);
+//   squareFeet = squareFeet.toFixed(2);
+//   console.log(`The area of the room is ${squareMeters} square meters (${squareFeet} square feet).`)
+// }
+
+// tip calculator 
+// console.log("What is the bill?"); 
+// let bill = parseFloat(readlineSync.prompt(), 10);
+
+// console.log("What is the tip percentage?"); 
+// let tipPercent = parseFloat(readlineSync.prompt(), 10); 
+
+// let tip = bill / 100 * tipPercent;
+// let total = tip + bill; 
+
+// console.log(`The tip is $${tip.toFixed(2)}`);
+// console.log(`The total is $${total.toFixed(2)}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
