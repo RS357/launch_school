84035eeb2f7df3e440802b9394cf8367aecdd127
Riz Syed <rizwansyed876@gmail.com>
2021-11-14 22:08:46 +0000

    file added

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -1,34 +1,53 @@
-const readline = require("readline-sync"); 
+const readline = require("readline-sync");
 
-function prompt(message) { 
+function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-prompt("Welcome to Calculator!"); 
+function invalidNumber(number) {
+  return number.trimStart() === "" || Number.isNaN(Number(number));
+}
 
-prompt("What's the first number?"); 
-let number1 = readline.question(); 
+prompt("Welcome to Calculator!");
 
-prompt("What's the second number?"); 
+prompt("What's the first number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt("What's the second number?");
 let number2 = readline.question();
 
-prompt("What operation would you like to perform?\n 1) Add 2) Subtract 3) Multiply 4) Divide"); 
-let operation = readline.question(); 
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = readline.question();
+}
 
-let output; 
-switch(operation) {
+prompt("What operation would you like to perform?\n 1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+
+while (!["1", "2", "3", "4"].includes(operation)) {
+  prompt("Must choose 1, 2, 3 or 4");
+  operation = readline.question();
+}
+
+let output;
+switch (operation) {
   case "1":   // "1" represents addition
     output = Number(number1) + Number(number2);
     break;
-  case "2": // "2" represents subtraction 
+  case "2": // "2" represents subtraction
     output = Number(number1) - Number(number2);
     break;
   case "3": // "3" represents multiplication
     output = Number(number1) * Number(number2);
     break;
-  case "4": // "4" represents division 
+  case "4": // "4" represents division
     output = Number(number1) / Number(number2);
-    break; 
+    break;
 }
 
-prompt(`The result is: ${output}`)
+prompt(`The result is: ${output}`);
