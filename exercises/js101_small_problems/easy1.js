9b17aa1694c9f7759855e2b78e46ec175a453cd2
Riz <riz@nate.tech>
2021-11-15 22:24:48 +0000

    file added

--- exercises/js101_small_problems/easy1.js
+++ exercises/js101_small_problems/easy1.js
@@ -12,3 +12,82 @@
 
 //   console.log(number);
 // }
+
+// How Big is the Room?
+// let readlineSync = require("readline-sync");
+// const SQMETERS_TO_SQFEET = 10.7639;
+// console.log("Enter the length of the room in meters:");
+// let length = readlineSync.prompt();
+// length = parseInt(length, 10);
+// console.log("Enter the width of the room in meters:");
+// let width = readlineSync.prompt();
+// width = parseInt(width, 10);
+// let areaInMeters = (length * width);
+// let areaInFeet = (areaInMeters * SQMETERS_TO_SQFEET);
+// console.log(
+//   `The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet).`
+// );
+
+// Sum or Product of Consecutive Integers
+// function computeSum(targetNum) {
+//   let total = 0;
+//   for (let num = 1; num <= targetNum; num += 1) {
+//     total += num;
+//   }
+//   return total;
+// }
+// function computeProduct(targetNum) {
+//   let total = 1;
+//   for (let num = 1; num <= targetNum; num += 1) {
+//     total *= num;
+//   }
+//   return total;
+// }
+// let readlineSync = require("readline-sync");
+// console.log("Please enter an integer greater than 0");
+// let number = parseInt(readlineSync.prompt(), 10);
+// console.log("Enter 's' to compute the sum, 'p' to compute the product.");
+// let operation = readlineSync.prompt();
+// if (operation === "s") {
+//   let sum = computeSum(number);
+//   console.log(`The sum of the integers between 1 and ${number} is ${sum}.`);
+// } else if (operation === "p") {
+//   let product = computeProduct(number);
+//   console.log(
+//     `The product of the integers between 1 and ${number} is ${product}.`
+//   );
+// } else {
+//   console.log("Oops. Unknown operation.");
+// }
+
+// Short Long Short
+// let shortLongShort = (str1, str2) => {
+//   if (str1.length > str2.length) {
+//     return `${str2}${str1}${str2}`;
+//   } else {
+//     return `${str1}${str2}${str1}`;
+//   }
+// };
+// console.log(shortLongShort('abc', 'defgh'));    // "abcdefghabc"
+// console.log(shortLongShort('abcde', 'fgh'));    // "fghabcdefgh"
+// console.log(shortLongShort('', 'xyz'));         // "xyz"
+
+// Leap Years (Part 1)
+// let isLeapYear = year => {
+//   if ((year % 4 === 0) && (year % 100 !== 0)) return true;
+//   if ((year % 100 === 0) && (year % 400 === 0)) return true;
+//   return false;
+// };
+
+// Leap Years (Part 2)
+// function isLeapYear(year) {
+//   if ((year < 1752) && (year % 4 === 0)) return true;
+//   if ((year < 1752) && (year % 4 !== 0)) return false;
+//   if (year % 400 === 0) {
+//     return true;
+//   } else if (year % 100 === 0) {
+//     return false;
+//   } else {
+//     return year % 4 === 0;
+//   }
+// }
