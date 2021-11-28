0a10c80c7c1a5629006dead1d1d02e13be612b64
Riz <riz@nate.tech>
2021-11-28 23:58:30 +0000

    code added

--- exercises/js101_small_problems/easy2.js
+++ exercises/js101_small_problems/easy2.js
@@ -99,3 +99,114 @@ let readlineSync = require("readline-sync");
 //   });
 //   return returnArr;
 // };
+
+
+// Convert a string to a number
+// let createNumObj = () => {
+//   let numObj = {
+//     0: 0,
+//     1: 1,
+//     2: 2,
+//     3: 3,
+//     4: 4,
+//     5: 5,
+//     6: 6,
+//     7: 7,
+//     8: 8,
+//     9: 9,
+//   };
+//   return numObj;
+// };
+
+// let stringToInteger = str => {
+//   let numObj = createNumObj();
+//   let total = 0;
+//   let counter = 0;
+//   for (let ind = str.length - 1; ind >= 0; ind -= 1) {
+//     total += (numObj[str[ind]]) * (10 ** counter);
+//     counter += 1;
+//   }
+//   return total;
+// };
+// console.log(stringToInteger("4321") === 4321); // logs true
+// console.log(stringToInteger("570") === 570); // logs true
+
+// let createHexNumObj = () => {
+//   let numObj =  {
+//     1: 1,
+//     2: 2,
+//     3: 3,
+//     4: 4,
+//     5: 5,
+//     6: 6,
+//     7: 7,
+//     8: 8,
+//     9: 9,
+//     A: 10,
+//     B: 11,
+//     C: 12,
+//     D: 13,
+//     E: 14,
+//     F: 15
+//   };
+//   return numObj;
+// };
+
+// let hexadecimalToInteger = str => {
+//   let numObj = createHexNumObj();
+//   let total = 0;
+//   let counter = 0;
+//   for (let ind = str.length - 1; ind >= 0; ind -= 1) {
+//     total += (numObj[str.toUpperCase()[ind]]) * (16 ** counter);
+//     counter += 1;
+//   }
+//   return total;
+// };
+
+// console.log(hexadecimalToInteger('4D9f') === 19871);
+
+// Convert a String to a Signed Number
+
+// let stringToSignedInteger = str => {
+//   if (str[0] === "+") return stringToInteger(str.slice(1));
+//   if (str[0] === "-") return -stringToInteger(str.slice(1));
+//   return stringToInteger(str);
+// };
+
+// console.log(stringToSignedInteger("4321") === 4321); // logs true
+// console.log(stringToSignedInteger("-570") === -570); // logs true
+// console.log(stringToSignedInteger("+100") === 100); // logs true
+
+// Convert a Number to a String!
+let integerToString = num => {
+  let str = "";
+  const DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  if (num === 0) return "0";
+  while (num > 0) {
+    let digit = num % 10;
+    str = DIGITS[digit] + str;
+    num = Math.trunc(num / 10);
+  }
+  return str;
+};
+
+// console.log(integerToString(4321));        // "4321"
+// console.log(integerToString(0));           // "0"
+// console.log(integerToString(5000));        // "5000"
+// console.log(integerToString(1234567890));  // "1234567890"
+
+// Convert Signed Number to a String
+// let signedIntegerToString = num => {
+//   switch (true) {
+//     case num === 0:
+//       return "0";
+//     case num > 0:
+//       return `+${integerToString(num)}`;
+//     case num < 0:
+//       return `-${integerToString(-num)}`;
+//   }
+// };
+
+// console.log(signedIntegerToString(4321) === "+4321");
+// console.log(signedIntegerToString(-123) === "-123");
+// console.log(signedIntegerToString(0) === "0");
