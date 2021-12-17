3a9e380a147aba6342f3bb60280a9a2305fe8d54
Riz Syed <rizwansyed876@gmail.com>
2021-12-17 05:06:56 +0000

    stuff changed

--- js101/lesson_2/rock_paper_scissors.js
+++ js101/lesson_2/rock_paper_scissors.js
@@ -1,124 +1,83 @@
 const readline = require("readline-sync");
 const VALID_CHOICES = {
-  rock: "r",
-  paper: "p",
-  scissors: "sc",
-  lizard: "l",
-  spock: "sp"
+  r: "rock",
+  p: "paper",
+  sc: "scissors",
+  l: "lizard",
+  sp: "spock"
 };
 
-let computerWinCount = 0;
-let humanWinCount = 0;
+const WINNING_COMBOS = {
+  rock:     ["scissors",    "lizard"],
+  paper:    ["rock",        "spock"],
+  scissors: ["paper",       "lizard"],
+  lizard:   ["spock",       "paper"],
+  spock:    ["scissors",    "rock"]
+};
 
-function displayWinner(choice, computerChoice) {
+const GOAL_WINS = 3;
+
+let scores = {
+  player: 0,
+  computer: 0
+};
+
+function playerWins(choice, computerChoice) {
+  return WINNING_COMBOS[choice].includes(computerChoice);
+}
+
+function result(choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
-  switch (true) {
-    // Winning cases
-    case (choice === "rock" && computerChoice === "lizard"):
-      prompt("You win!");
-      return "human";
-    case (choice === "rock" && computerChoice === "scissors"):
-      prompt("You win!");
-      return "human";
-    case (choice === "lizard" && computerChoice === "spock"):
-      prompt("You win!");
-      return "human";
-    case (choice === "lizard" && computerChoice === "paper"):
-      prompt("You win!");
-      return "human";
-    case (choice === "spock" && computerChoice === "scissors"):
-      prompt("You win!");
-      return "human";
-    case (choice === "spock" && computerChoice === "rock"):
-      prompt("You win!");
-      return "human";
-    case (choice === "scissors" && computerChoice === "paper"):
-      prompt("You win!");
-      return "human";
-    case (choice === "scissors" && computerChoice === "lizard"):
-      prompt("You win!");
-      return "human";
-    case (choice === "paper" && computerChoice === "rock"):
-      prompt("You win!");
-      return "human";
-    case (choice === "paper" && computerChoice === "spock"):
-      prompt("You win!");
-      return "human";
-    case (choice === "rock" && computerChoice === "lizard"):
-      prompt("You win!");
-      return "human";
-    case (choice === "rock" && computerChoice === "scissors"):
-      prompt("You win!");
-      return "human";
-    case (choice === "lizard" && computerChoice === "spock"):
-      prompt("You win!");
-      return "human";
-    case (choice === "lizard" && computerChoice === "paper"):
-      prompt("You win!");
-      return "human";
-    case (choice === "spock" && computerChoice === "scissors"):
-      prompt("You win!");
-      return "human";
-    case (choice === "spock" && computerChoice === "rock"):
-      prompt("You win!");
-      return "human";
-    case (choice === "scissors" && computerChoice === "paper"):
-      prompt("You win!");
-      return "human";
-    case (choice === "scissors" && computerChoice === "lizard"):
-      prompt("You win!");
-      return "human";
-    case (choice === "paper" && computerChoice === "rock"):
-      prompt("You win!");
-      return "human";
-    case (choice === "paper" && computerChoice === "spock"):
-      prompt("You win!");
-      return "human";
-    //Losing Cases
-    case (choice === "lizard" && computerChoice === "rock"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "scissors" && computerChoice === "rock"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "spock" && computerChoice === "lizard"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "paper" && computerChoice === "lizard"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "scissors" && computerChoice === "spock"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "rock" && computerChoice === "spock"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "paper" && computerChoice === "scissors"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "lizard" && computerChoice === "scissors"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "rock" && computerChoice === "paper"):
-      prompt("Computer wins!");
-      return "computer";
-    case (choice === "spock" && computerChoice === "paper"):
-      prompt("Computer wins!");
-      return "computer";
-    default:
-      prompt("It's a tie");
+  if (playerWins(choice, computerChoice)) {
+    return "player";
+  } else if (choice === computerChoice) {
+    return "tie";
+  } else {
+    return "computer";
   }
-};
+}
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-while (true) {
+function incrementScore(winner) {
+  winner += 1;
+}
+
+function setScore(winner) {
+  if (winner === "player") {
+    incrementScore(scores.player);
+  } else if (winner === "computer") {
+    incrementScore(scores.computer);
+  }
+}
+
+function printScore (winner) {
+  if (winner === "player") {
+    prompt("You win!");
+  } else if (winner === "computer") {
+    prompt("You lose");
+  } else {
+    prompt("It's a tie!");
+  }
+  prompt(`You've won ${scores.player} games so far.`);
+  prompt(`computer has won ${scores.computer} games so far`);
+}
+
+function randomChoice() {
+  let validChoicesVals = Object.values(VALID_CHOICES);
+  let randomIndex = Math.round(Math.random() * validChoicesVals.length);
+  if (randomIndex === validChoicesVals.length) {
+    randomIndex = validChoicesVals.length - 1;
+  }
+  return validChoicesVals[randomIndex];
+}
+
+function retrieveChoice() {
   let validChoicesKeys = Object.keys(VALID_CHOICES);
   let validChoicesVals = Object.values(VALID_CHOICES);
-
-  prompt(`Choose one: ${validChoicesKeys.join(", ")}`);
+  prompt(`Choose one: ${validChoicesVals.join(", ")}`);
   let choice = readline.question();
 
   while (!validChoicesKeys.includes(choice) &&
@@ -129,19 +88,26 @@ while (true) {
       prompt("That's not a valid choice");
     }
     choice = readline.question();
+    if (choice.length <= 2) choice = VALID_CHOICES[choice];
   }
+  if (choice.length <= 2) choice = VALID_CHOICES[choice];
+  return choice;
+}
 
-  let randomIndex = Math.round(Math.random() * validChoicesKeys.length);
-  if (randomIndex === 5) randomIndex = 4;
-  console.log(`randomIndex is ${randomIndex}`);
-  console.log(`validChoicesVals[randomIndex] is: ${validChoicesKeys[randomIndex]}`);
-  let computerChoice = validChoicesKeys[randomIndex];
+while (true) {
+  let choice = retrieveChoice();
 
-  let winner = displayWinner(choice, computerChoice);
-  if (winner === "human") humanWinCount += 1;
-  if (winner === "computer") computerWinCount += 1;
+  let computerChoice = randomChoice();
 
-  if (humanWinCount === 3 || computerWinCount === 3) {
+  let winner = result(choice, computerChoice);
+
+  setScore(winner);
+
+  printScore(winner);
+
+  console.log(`scores: ${JSON.stringify(scores)}`);
+
+  if (scores.player === GOAL_WINS || scores.computer === GOAL_WINS) {
     prompt(`best of three reached - ${winner} wins!`);
     prompt("Game over!");
     break;
