ab31a36c1cb45024084416c674fcb6b99d7a4471
Riz Syed <rizwansyed876@gmail.com>
2022-03-03 21:35:41 +0000

    added;

---
+++ exercises/js101_small_problems/medium2.js
@@ -0,0 +1,17 @@
+// Lettercase Percentage Ratio
+
+// let letterPercentages = str => {
+//   let count = str.length;
+//   let totals = {
+//     lowercase: (((str.match(/[a-z]/g) || []).length / count) * 100).toFixed(
+//       2
+//     ),
+//     uppercase: (((str.match(/[A-Z]/g) || []).length / count) * 100).toFixed(
+//       2
+//     ),
+//     neither: (((str.match(/[^a-z]/gi) || []).length / count) * 100).toFixed(
+//       2
+//     ),
+//   };
+//   return totals;
+// };
