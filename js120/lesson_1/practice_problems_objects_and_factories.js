703cae3afb3b81781e8433ae6a8ae461cb98e773
Riz Syed <rizwansyed876@gmail.com>
2022-09-25 18:58:14 +0100

    added

--- js120/lesson_1/practice_problems_objects_and_factories.js
+++ js120/lesson_1/practice_problems_objects_and_factories.js
@@ -1,35 +1,19 @@
-let createBook = function(title, author, read = false) {
+let createBook = (title, author) => {
   return {
     title,
     author,
-    read,
+    read: false,
 
     getDescription() {
-      return `${this.title} was written by ${this.author}. ` +
-      `I ${this.read ? 'have' : "haven't"} read it`;
-    },
-
-    readBook() {
-      this.read = true;
-    },
+      return `${title} was written by ${author}`;
+    }
   };
 };
 
-let book1 = createBook('Mythos', 'Stephen Fry');
-let book2 = createBook('Me Talk Pretty One Day', 'David Sedaris');
-let book3 = createBook("Aunts aren't Gentlemen", 'PG Wodehouse');
+let book1 = createBook("Mythos", "Stephen Fry");
+let book2 = createBook("Me Talk Pretty One Day", "David Sedaris");
+let book3 = createBook("Aunts aren't Gentlemen", "PG Wodehouse");
 
-console.log(book1.getDescription());  // "Mythos was written by Stephen Fry."
-book1.readBook();
-console.log(book1.getDescription());  // "Mythos was written by Stephen Fry."
-console.log(book2.getDescription());  // "Me Talk Pretty One Day was written by David Sedaris."
-console.log(book3.getDescription());  // "Aunts aren't Gentlemen was written by PG Wodehouse"
-
-
-// console.log(book1.read); // => false
-// console.log(book2.read); // => false
-// console.log(book3.read); // => false
-
-// console.log(book1.read); // => false
-// book1.readBook();
-// console.log(book1.read); // => true
+console.log(book1.read); // => false
+console.log(book2.read); // => false
+console.log(book3.read); // => false
