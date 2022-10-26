733936899309bc5b6a0c695dee7f4ff6e04a247e
riz <rizwansyed876@gmail.com>
2022-10-26 16:37:49 +0100

    added

---
+++ exercises/js120/easy.js
@@ -0,0 +1,129 @@
+// Rectangles 
+
+// class Rectangle {
+//   constructor(width, length) {
+//     this.width = width;
+//     this.length = length;
+//   }
+
+//   getWidth() {
+//     return this.width;
+//   }
+
+//   getLength() {
+//     return this.length;
+//   }
+
+//   getArea() {
+//     return this.length * this.width;
+//   }
+// }
+
+// let rect = new Rectangle(4, 5);
+
+// console.log(rect.getWidth()); // 4
+// console.log(rect.getLength()); // 5
+// console.log(rect.getArea()); // 20
+
+// Rectangles and Squares
+
+// class Rectangle {
+//   constructor(width, length) {
+//     this.width = width;
+//     this.length = length;
+//   }
+
+//   getWidth() {
+//     return this.width;
+//   }
+
+//   getLength() {
+//     return this.length;
+//   }
+
+//   getArea() {
+//     return this.width * this.length;
+//   }
+// }
+
+// class Square extends Rectangle {
+//   constructor(length) {
+//     super(length, length);
+//   }
+
+// }
+
+// let square = new Square(5);
+// console.log(`area of square = ${square.getArea()}`); // area of square = 25
+
+// Fake Cat 
+
+// class Cat {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   speaks() {
+//     return `${this.name} says meowwww.`;
+//   }
+// }
+
+// let fakeCat = Object.create(Cat.prototype);
+// console.log(fakeCat instanceof Cat); // logs true
+// console.log(fakeCat.name);           // logs undefined
+// console.log(fakeCat.speaks());       // logs undefined says meowwww.
+
+// Complete the Program - Cats!
+
+// class Pet {
+//   constructor(name, age, colours) {
+//     this.name = name;
+//     this.age = age;
+//     this.colours = colours;
+//   }
+// }
+
+// class Cat extends Pet {
+//   info() {
+//     console.log(`My cat ${this.name} is ${this.age} years old and has ${this.colours} fur.`);
+//   }
+// }
+
+// let pudding = new Cat('Pudding', 7, 'black and white');
+// let butterscotch = new Cat('Butterscotch', 10, 'tan and white');
+
+// console.log(pudding.info());
+// console.log(butterscotch.info());
+
+// Animals 
+
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old and ${this.status}.`;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status, legs = 4, species = 'cat') {
+    super(name, age, legs, species, status);
+  }
+
+  introduce() {
+    return `${Animal.prototype.introduce.call(this)} Meow meow!`;
+  }
+
+}
+
+class Dog extends Animal {}
+
+let cat = new Cat("Pepe", 2, "happy");
+console.log(cat.introduce() === "Hello, my name is Pepe and I am 2 years old and happy. Meow meow!");
+console.log(cat.introduce());
+
+// logs true
