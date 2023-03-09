8876a51a828940aa9636883447adc819fd60c648
Riz Syed <rizwansyed876@gmail.com>
2023-03-09 23:15:46 +0000

    added

--- js130/lesson1/practice_problems1.js
+++ js130/lesson1/practice_problems1.js
@@ -1,10 +1,10 @@
-// // 1)
+// 1)
 
-// function filter(arr, callback) {
+// function filter(array, callback) {
 //   let filtered = [];
-//   for (let idx = 0; idx < arr.length; idx += 1) {
-//     if (callback(arr[idx])) {
-//       filtered.push(arr[idx]);
+//   for (let idx = 0; idx < array.length; idx += 1) {
+//     if (callback(array[idx])) {
+//       filtered.push(array[idx]);
 //     }
 //   }
 //   return filtered;
@@ -19,13 +19,12 @@
 // console.log(filter(values, value => typeof value === "string"));
 // // => [ 'abc', 'xyz' ]
 
+
 // 2)
-
-// function map(arr, callback) {
+// function map(array, callback) {
 //   let mapped = [];
-
-//   for (let idx = 0; idx < arr.length; idx += 1) {
-//     mapped.push(callback(arr[idx]));
+//   for (let idx = 0; idx < array.length; idx += 1) {
+//     mapped.push(callback(array[idx]));
 //   }
 //   return mapped;
 // }
@@ -40,17 +39,21 @@
 // console.log(map(values, value => String(value)));
 // // => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
 
+// function reduce(array, callback, firstVal = null) {
+//   let startIdx = 0;
+//   let finalVal = firstVal;
 
-// function reduce(arr, callBack, initialVal) {
-//   let accumulated = initialVal ? initialVal : arr[0];
-//   let startIdx = initialVal ? 0 : 1
-//   for (let idx = startIdx; idx < arr.length; idx += 1) {
-//     accumulated = callBack(accumulated, arr[idx]);
-//     // console.log(accumulated);
+//   if (firstVal === null) {
+//     startIdx = 1;
+//     finalVal = array[0];
 //   }
-//   return accumulated;
-// }
 
+//   for (let idx = startIdx; idx < array.length; idx += 1) {
+//     finalVal = callback(finalVal, array[idx]);
+//   }
+
+//   return finalVal;
+// }
 
 // let numbers = [1, 2, 3, 4, 5];
 // console.log(reduce(numbers, (accum, number) => accum + number));   // => 15
@@ -58,67 +61,31 @@
 // console.log(reduce(numbers, (prod, number) => prod * number, 3));  // => 360
 // console.log(reduce([], (accum, number) => accum + number, 10));    // => 10
 // console.log(reduce([], (accum, number) => accum + number));
-// // // // => undefined
+// // => undefined
 
 // let stooges = ["Mo", "Larry", "Curly"];
 // console.log(reduce(stooges, (reversedStooges, stooge) => {
 //   reversedStooges.unshift(stooge);
 //   return reversedStooges;
 // }, []));
-// // => ["Curly", "Larry", "Mo"]
+// => ["Curly", "Larry", "Mo"]
 
 
-// 2)
-// function filter(arr, filterCallBack) {
-//   function reduceCallBack (accum, currentVal) {
-//     if (filterCallBack(currentVal)) {
-//       accum.push(currentVal);
-//       return accum;
-//     } else return accum;
-//   }
-//   return arr.reduce(reduceCallBack, []);
-// }
-
-// let numbers = [1, 2, 3, 4, 5];
-// console.log(filter(numbers, number => number > 3)); // => [ 4, 5 ]
-// console.log(filter(numbers, number => number < 0)); // => []
-// console.log(filter(numbers, () => true));           // => [ 1, 2, 3, 4, 5 ]
-
-// let values = [1, "abc", null, true, undefined, "xyz"];
-// console.log(filter(values, value => typeof value === "string"));
-// // => [ 'abc', 'xyz' ]
-
-// // 3)
-
-// function map(arr, mapCallBack) {
-//   function reduceCallBack (accum, currentVal) {
-//     accum.push(mapCallBack(currentVal));
-//     return accum;
-//   }
-//   return arr.reduce(reduceCallBack, []);
-// }
-
-// let numbers = [1, 2, 3, 4, 5];
-// console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
-// console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
-// console.log(map(numbers, () => false));
-// // => [ false, false, false, false, false ]
-
-// let values = [1, "abc", null, true, undefined, "xyz"];
-// console.log(map(values, value => String(value)));
-// // => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
-
-// Object forEach
-
-function objForEach(object, callback) {
-  for (let property in object) {
-    if (object.hasOwnProperty(property)) {
-      callback(property, object[property]);
+function mapper(array, callback) {
+  return array.reduce((accum, arrVal) => {
+    if (callback(arrVal)) {
+      accum.push(callback(arrVal));
     }
-  }
+    return accum;
+  }, []);
 }
 
-let obj = { foo: 1, bar: 2, qux: 3 };
-objForEach(obj, (property, value) => {
-  console.log(`the value of ${property} is ${value}`);
-});
+function mapper(array, callback) {
+  return array.reduce((accum, arrVal) => {
+    accum.push(callback(arrVal));
+    return accum;
+  }, []);
+}
+
+let arr = [1, 2, 3];
+console.log(mapper(arr, val => val * 2));
