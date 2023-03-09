c3bd6def688ca9566bce9e92dc9d648699de1a37
riz <rizwansyed876@gmail.com>
2023-03-09 17:33:20 +0000

    added

--- js130/lesson1/practice_problems1.js
+++ js130/lesson1/practice_problems1.js
@@ -108,17 +108,3 @@
 // console.log(map(values, value => String(value)));
 // // => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
 
-// Object forEach
-
-function objForEach(object, callback) {
-  for (let property in object) {
-    if (object.hasOwnProperty(property)) {
-      callback(property, object[property]);
-    }
-  }
-}
-
-let obj = { foo: 1, bar: 2, qux: 3 };
-objForEach(obj, (property, value) => {
-  console.log(`the value of ${property} is ${value}`);
-});
