ff39680ae3ffacacb531f2f07b1c0d7ac2f07212
Riz Syed <rizwansyed876@gmail.com>
2023-03-17 12:25:17 +0000

    added

--- exercises/js_challenges/medium_challenges/simple-linked-list/simple-linked-list.js
+++ exercises/js_challenges/medium_challenges/simple-linked-list/simple-linked-list.js
@@ -1,34 +1,191 @@
+class Element {
+  constructor(...args) {
+    this.data = args[0];
+    this.nextNode = null;
+    if (args.length === 2) {
+      if (args[1] instanceof Element) {
+        this.nextNode = args[1];
+      }
+    }
+  }
+
+  datum() {
+    return this.data;
+  }
+
+  isTail() {
+    return this.nextNode === null;
+  }
+  next() {
+    return this.nextNode;
+  }
+}
+
+class SimpleLinkedList {
+  constructor() {
+    this.headNode = null;
+  }
+
+  static fromArray(arr) {
+    let newLinkedList = new SimpleLinkedList();
+    if (arr === null || arr.length === 0) {
+      return newLinkedList;
+    }
+    arr.slice().reverse().forEach(val => newLinkedList.push(val));
+    return newLinkedList;
+  }
+
+  size() {
+    let counter = 0;
+    let currentNode = this.head();
+    while (currentNode !== null) {
+      counter += 1;
+      currentNode = currentNode.next();
+    }
+    return counter;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+  push(val) {
+    let element = new Element(val, this.head());
+    this.headNode = element;
+  }
+
+  peek() {
+    if (this.headNode === null) return null;
+    return this.headNode.datum();
+  }
+
+  head() {
+    return this.headNode;
+  }
+
+  pop() {
+    let datum = this.peek();
+    this.headNode = this.head().next();
+    return datum;
+  }
+
+  toArray() {
+    if (this.head() === null) return [];
+    let arr = [];
+    let currentNode = this.head();
+    while (currentNode !== null) {
+      arr.push(currentNode.datum());
+      currentNode = currentNode.next();
+    }
+    return arr;
+  }
+
+  reverse() {
+    let datumArr = this.toArray();
+    let reversedDatumArr = datumArr.reverse();
+    return SimpleLinkedList.fromArray(reversedDatumArr);
+  }
+
+}
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let list = SimpleLinkedList.fromArray(data);
+// console.log(list.reverse().toArray());
+data.reverse();
+console.log(data);
+module.exports = {
+  Element,
+  SimpleLinkedList
+};
+
 /*
-Problem: Write a class that implements a linked list.
+/*eslint-disable*/
+/*
+Algo:
 
-Class Element:
-  constructor:
-    input: a number and instance of element, or just a number 
-    assign to instance property datum the number passed into constructor
-    if element instance is passed in, assign it to the instance property 
-      next. 
+class SimpleLinkedList:
+  instance identifiers:
 
-  instance method datum:
-    input: nothing
-    output: the Element instance's internal 'datum' property 
+    function reverse: 
+      assign result of calling toArray on this instance, datumArr,
+      reverse datumArr, reversedDatumArr
+      return new simplelinkedlist object by passing reversedDatumArr to fromArray and returning what it returns
 
-  instance method isTail
-    input: nothing
-    output: true or false 
-  instance method next
-    input: nothing 
-    output: the element assigned to the instance property next 
+    function toArray:
+      if instance property listSize is 0 return empty array  
 
-class SimpleLinkedList
-  static methid fromArray
 
-  instance method size
-  instance method isEmpty 
-  instance method peek
-  instance method head 
-  instance method push  
-  instance method pop
-  instance method toArray
-  instance method reverse
+    function pop:
+      assign instane property headNode to variable, currentHeaDNode
+      if instance property listSize is 1:
+        assign headNode to null
+        assign listSize to 0
+        return currentHeadNode datum method return val
+      else:
+        assign instance headNode property to returned value of calling next on object
+          assigned to instance headNode property 
+          decrement listSize instance property by 1
+        return result of calling datum on currentHeadode
 
+    function head 
+      return instance property headNode
+
+    function push // given val
+      create new Element instance, with val as arg passed in, element 
+      if headnode is not null:
+        assign element's next property to value assigned to headNode 
+      assign headNode to element
+      increment instance property listSize by 1 
+
+    property headNode - assign to null
+
+    property size
+
+    function size 
+      return instnace property size
+
+    function isEmpty 
+      return true if instance property size is 0
+
+    function size 
+
+    function peek 
+      if instance property instance property headNode is null return null
+      call datum instance method on instance property headNode and return result.
+
+    function pop
+    function toArray
+    function reverse
+
+
+
+class Element: 
+
+  instance identifiers:
+    property data (first arg to constructor)
+
+    property nextNode - assigned to null
+
+    function datum 
+      output: instance property datum
+
+    functtion isTail
+      return true if nextNode is null
+
+    function next 
+      return nextNode instance property
+
+    function constructor: // given args
+      if only 1 value in args
+        assigns datum instance property to the arg
+      else if more than 1 value in args:
+        set instance property next to be the arg that is an instance of element 
+        assigns datum instance property to arg that is not instance of element 
+
+
+  static identifiers:
+
+  Problem: Write a class that implements a linked list.
+
+Rules:
+  Each 'element' contains data and a next field pointing to the next element in the linked list 
 */
--- exercises/js_challenges/medium_challenges/simple-linked-list/simple-linked-list.test.js
+++ exercises/js_challenges/medium_challenges/simple-linked-list/simple-linked-list.test.js
@@ -6,57 +6,57 @@ describe("Simple Linked List", () => {
     expect(element.datum()).toBe(1);
   });
 
-  xtest("element tail", () => {
+  test("element tail", () => {
     let element = new Element(1);
     expect(element.isTail()).toBe(true);
   });
 
-  xtest("element next default", () => {
+  test("element next default", () => {
     let element = new Element(1);
     expect(element.next()).toBe(null);
   });
 
-  xtest("element next initialization", () => {
+  test("element next initialization", () => {
     let element1 = new Element(1);
     let element2 = new Element(2, element1);
     expect(element2.next()).toEqual(element1);
   });
 
-  xtest("empty list size", () => {
+  test("empty list size", () => {
     let list = new SimpleLinkedList();
     expect(list.size()).toBe(0);
   });
 
-  xtest("empty list empty", () => {
+  test("empty list empty", () => {
     let list = new SimpleLinkedList();
     expect(list.isEmpty()).toBe(true);
   });
 
-  xtest("pushing element on list", () => {
+  test("pushing element on list", () => {
     let list = new SimpleLinkedList();
     list.push(1);
     expect(list.size()).toBe(1);
   });
 
-  xtest("empty list 1 element", () => {
+  test("empty list 1 element", () => {
     let list = new SimpleLinkedList();
     list.push(1);
     expect(list.isEmpty()).toBe(false);
   });
 
-  xtest("peeking at list", () => {
+  test("peeking at list", () => {
     let list = new SimpleLinkedList();
     list.push(1);
     expect(list.size()).toBe(1);
     expect(list.peek()).toBe(1);
   });
 
-  xtest("peeking at empty list", () => {
+  test("peeking at empty list", () => {
     let list = new SimpleLinkedList();
     expect(list.peek()).toBe(null);
   });
 
-  xtest("access head element", () => {
+  test("access head element", () => {
     let list = new SimpleLinkedList();
     list.push(1);
     let head = list.head();
@@ -65,7 +65,7 @@ describe("Simple Linked List", () => {
     expect(head.isTail()).toBe(true);
   });
 
-  xtest("items are stacked", () => {
+  test("items are stacked", () => {
     let list = new SimpleLinkedList();
     list.push(1);
     list.push(2);
@@ -75,7 +75,7 @@ describe("Simple Linked List", () => {
     expect(list.head().next().datum()).toBe(1);
   });
 
-  xtest("push 10 items", () => {
+  test("push 10 items", () => {
     let list = new SimpleLinkedList();
 
     for (let datum = 1; datum <= 10; datum++) {
@@ -86,7 +86,7 @@ describe("Simple Linked List", () => {
     expect(list.peek()).toBe(10);
   });
 
-  xtest("pop one item", () => {
+  test("pop one item", () => {
     let list = new SimpleLinkedList();
     list.push(1);
 
@@ -94,7 +94,7 @@ describe("Simple Linked List", () => {
     expect(list.size()).toBe(0);
   });
 
-  xtest("popping frenzy", () => {
+  test("popping frenzy", () => {
     let list = new SimpleLinkedList();
 
     for (let datum = 1; datum <= 10; datum++) {
@@ -109,27 +109,27 @@ describe("Simple Linked List", () => {
     expect(list.peek()).toBe(4);
   });
 
-  xtest("from a: empty array", () => {
+  test("from a: empty array", () => {
     let list = SimpleLinkedList.fromArray([]);
 
     expect(list.size()).toBe(0);
     expect(list.peek()).toBe(null);
   });
 
-  xtest("from a: null", () => {
+  test("from a: null", () => {
     let list = SimpleLinkedList.fromArray(null);
     expect(list.size()).toBe(0);
     expect(list.peek()).toBe(null);
   });
 
-  xtest("from a: 2 element array", () => {
+  test("from a: 2 element array", () => {
     let list = SimpleLinkedList.fromArray([1, 2]);
     expect(list.size()).toBe(2);
     expect(list.head().datum()).toBe(1);
     expect(list.head().next().datum()).toBe(2);
   });
 
-  xtest("from a: 10 item array", () => {
+  test("from a: 10 item array", () => {
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let list = SimpleLinkedList.fromArray(arr);
     expect(list.size()).toBe(10);
@@ -138,22 +138,22 @@ describe("Simple Linked List", () => {
       .next().next().next().next().next().datum()).toBe(10);
   });
 
-  xtest("to a: empty list", () => {
+  test("to a: empty list", () => {
     let list = new SimpleLinkedList();
     expect(list.toArray()).toEqual([]);
   });
 
-  xtest("to a: of 1 element list ", () => {
+  test("to a: of 1 element list ", () => {
     let list = SimpleLinkedList.fromArray([1]).toArray();
     expect(list).toEqual([1]);
   });
 
-  xtest("to a: of a 2 element list", () => {
+  test("to a: of a 2 element list", () => {
     let list = SimpleLinkedList.fromArray([1, 2]).toArray();
     expect(list).toEqual([1, 2]);
   });
 
-  xtest("reverse 2 element list", () => {
+  test("reverse 2 element list", () => {
     let list = SimpleLinkedList.fromArray([1, 2]);
     // reversedList and list need not be the same object
     let reversedList = list.reverse();
@@ -163,13 +163,13 @@ describe("Simple Linked List", () => {
     expect(reversedList.head().next().isTail()).toBe(true);
   });
 
-  xtest("reverse 10 element list", () => {
+  test("reverse 10 element list", () => {
     let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let list = SimpleLinkedList.fromArray(data);
     expect(data.reverse()).toEqual(list.reverse().toArray());
   });
 
-  xtest("roundtrip 10 element array", () => {
+  test("roundtrip 10 element array", () => {
     let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let list = SimpleLinkedList.fromArray(data).toArray();
     expect(list).toEqual(data);
