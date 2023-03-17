2f7d2fb81ccb3150a41de9a28c40124fce0b60ae
riz <rizwansyed876@gmail.com>
2023-03-17 17:47:52 +0000

    added

--- js130/lesson5/example.js
+++ js130/lesson5/example.js
@@ -1,3 +1,7 @@
-// Questions to do again:
-
-// https://launchschool.com/gists/43161e36 <- problem 1, 
+function delayLog() {
+  for (let idx = 0; idx < 10; idx += 1) {
+    console.log('idx: ',idx);
+    setInterval(() => console.log(idx), 1000 * idx);
+  }
+}
+delayLog();
